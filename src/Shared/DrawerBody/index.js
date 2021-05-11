a21b16a0f5c7af9ba473c2cbbd53e6f75ce5bc90
edwarddevp <edwarddevp@gmail.com>
2021-05-11 19:31:23 -0400

    -Se reparo bug donde status bar se sobreponia al drawer -Se elimino Pantallas de ejmeplo Detail y Search

--- src/Shared/DrawerBody/index.js
+++ src/Shared/DrawerBody/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Drawer as KittenDrawer, DrawerItem, IndexPath  } from "@ui-kitten/components";
+import { StatusBar } from 'react-native';
 
 export const DrawerBody = ({ navigation, state }) => (
     <KittenDrawer
       selectedIndex={new IndexPath(state.index)}
-      onSelect={index => navigation.navigate(state.routeNames[index.row])}>
+      onSelect={index => navigation.navigate(state.routeNames[index.row])}
+      style={{ flex: 1, marginTop: StatusBar.currentHeight }}
+      >
       <DrawerItem title='Users' />
       <DrawerItem title='Orders' />
     </KittenDrawer>
