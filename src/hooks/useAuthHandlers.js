89e522ee004987937e38610eea7ba5c64efc4e2d
edwarddevp <edwarddevp@gmail.com>
2021-05-17 18:01:05 -0400

    -Separe la logica de auth a un hook diferente useAuthHandlers.js, dejando el hook useApiRequest.js solo para llamados que utilizen token -Se creo un hook llamado en App.js que verifica si es la primera vez que se corre la app con el fin de mostrar el onBoarding o no -En el hook de inicio tambien se confirma que el token del usuario este vigente y lo redirija al home en vez de al login -Tambien se agrego un componente para resolver un bug visual con la StatusBar en android -Se intalo una dependencia promise.allSettle polilfy

---
+++ src/hooks/useAuthHandlers.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import {API_URL} from "@env"
+import {REGISTER, SIGNIN} from "../lib/apiRoutes";
+
+export const useAuthHandlers = () => {
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState({})
+
+  const handleAuthRequest = async (path, params) => {
+    try {
+      setLoading(true)
+      const response = await fetch(API_URL + path, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(params)// body data type must match "Content-Type" header
+      });
+      const json = await response.json()
+      setLoading(false)
+      return json
+    } catch (e) {
+      setError(e)
+      setLoading(false)
+      console.log('%c e', 'background: #222; color: #bada55', e)
+    }
+  }
+
+  return {
+    handleLogin:  (params) => handleAuthRequest(SIGNIN,params),
+    handleRegister: (params) => handleAuthRequest(REGISTER,params) ,
+    loading,
+    error,
+  }
+};
